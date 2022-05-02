51be09f6e723c294140ec53298fa1ac00c3d5774
maurosaracini <95768222+maurosaracini@users.noreply.github.com>
2022-05-02 14:30:32 -0300

    Modulo
    Funcionalidad de alta baja y modificación de un Modulo

--- app/modulo/ModuloService.php
+++ app/modulo/ModuloService.php
@@ -20,16 +20,34 @@ class ModuloService
 
     public function updateModulo($params)
     {
-        $sqlQuery = "UPDATE FDPyR_modulo SET cuestionario_id=?, adjunto_id=?, orden=?, titulo=?, descripcion=?, puntaje=? modified_at = CURRENT_TIMESTAMP WHERE id_modulo=? AND deleted_at IS NULL";
-        $bindParams = [
-            $params['cuestionario_id'],
-            $params['adjunto_id'],
-            $params['orden'],
-            $params['titulo'],
-            $params['descripcion'],
-            $params['puntaje'],
-            $params['id_modulo']
-        ];
+        $bindParams = [];
+        $sqlQuery = "UPDATE FDPyR_modulo SET" ;
+        if (isset($params['cuestionario_id'])) {
+            $sqlQuery .= " cuestionario_id = ?,";
+            array_push($bindParams,$params['cuestionario_id']);
+        }
+        if (isset($params['adjunto_id'])) {
+            $sqlQuery .= " adjunto_id = ?,";
+            array_push($bindParams,$params['adjunto_id']);
+        }
+        if (isset($params['orden'])) {
+            $sqlQuery .= " orden = ?,";
+            array_push($bindParams,$params['orden']);
+        }
+        if (isset($params['titulo'])) {
+            $sqlQuery .= " titulo = ?,";
+            array_push($bindParams,$params['titulo']);
+        }
+        if (isset($params['descripcion'])) {
+            $sqlQuery .= " descripcion = ?,";
+            array_push($bindParams,$params['descripcion']);
+        }
+        if (isset($params['puntaje'])) {
+            $sqlQuery .= " puntaje = ?,";
+            array_push($bindParams,$params['puntaje']);
+        }
+        array_push($bindParams,$params['id_modulo']);
+        $sqlQuery .= " modified_at = CURRENT_TIMESTAMP WHERE id_modulo=? AND deleted_at IS NULL";
 
         $database = new BaseDatos;
         $database->connect();
